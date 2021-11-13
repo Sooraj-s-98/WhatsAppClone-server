93cbb77354788028ad41bea2bda8a795a42bb075
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2021-11-13 18:47:32 +0530

    server init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+var http = require("http");
+const app = express();
+const port = process.env.PORT || 5000;
+var server = http.createServer(app);
+var io = require("socket.io")(server);
+
+//middlewre
+app.use(express.json());
+
+io.on("connection", (socket) => {
+  console.log("connetetd");
+  console.log(socket.id, "has joined");
+
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log("server started");
+});
