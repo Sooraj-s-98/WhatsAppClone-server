147fa73a4e7baa8e70d1d8b55117e09522b6dc9c
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2021-11-20 10:20:09 +0530

    server connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 var http = require("http");
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 var server = http.createServer(app);
 var io = require("socket.io")(server);
 
@@ -11,9 +11,11 @@ app.use(express.json());
 io.on("connection", (socket) => {
   console.log("connetetd");
   console.log(socket.id, "has joined");
-
+  socket.on("/test",data=>{
+    console.log(data)
+  })
 });
 
 server.listen(port, "0.0.0.0", () => {
-  console.log("server started");
+  console.log(`server started ${port} `);
 });
